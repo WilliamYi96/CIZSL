74d2df7f7b0f42fd43b6311a32e8ccf66e6c4f16
kaiyi96 <williamyi96@gmail.com>
2020-06-19 16:45:34 +0800

    sc

---
+++ run-script4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=cizsl-reproduce-round4
+#SBATCH --nodes=8
+#SBATCH --time=48:00:00
+#SBATCH --partition=batch
+#SBATCH --mem=16GB
+#SBATCH --gres=gpu:8
+#SBATCH --constraint=[rtx2080ti]
+#SBATCH --array=1-10
+
+cd /home/yik/CIZSL/
+
+./run4.sh
